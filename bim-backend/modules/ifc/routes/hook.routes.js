b9244c60513896b94a3054ec3924f6d88c1760e2
TriLVH <levohuutri@gmail.com>
2024-09-10 21:54:47 +0700

    feature: update tiler for bim htkt

--- bim-backend/modules/ifc/routes/hook.routes.js
+++ bim-backend/modules/ifc/routes/hook.routes.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
-const { HookService } = require("../services/hook.service");
+const { HookService } = require('../services/hook.service');
 
-module.exports = async function(fastify, opts) {
-    fastify.post('/uploaded', async function (req, res) {
-        HookService.hookIFCModelUploaded(req.body.data);
-        return req.body;
-    })
-}
+module.exports = async function (fastify, opts) {
+  fastify.post('/uploaded', async function (req, res) {
+    HookService.hookIFCModelUploaded(req.body.data, req.body.source);
+    return req.body;
+  });
+};
--- bim-backend/modules/ifc/services/hook.service.js
+++ bim-backend/modules/ifc/services/hook.service.js
@@ -7,14 +7,21 @@ class HookService {
    * @param {*} ifcModelData {id, file_model: {url}}
    * @returns true if success
    */
-  static async hookIFCModelUploaded(ifcModelData) {
-    const fileData = { name: `${ifcModelData?.id}`, path: ifcModelData?.file_model[0].url, modelId: ifcModelData.id };
+  static async hookIFCModelUploaded(ifcModelData, source) {
+    const fileData = {
+      name: `${source}-${ifcModelData?.id}`,
+      path: ifcModelData?.file_model[0].url,
+      modelId: ifcModelData.id,
+    };
     // Execute tiling
     const ifcTilerHelper = new IFCTilerHelper(fileData);
     const ifcExtractor = new IFCExtractorHelper(fileData);
 
     // Execute props extractor
-    await Promise.all([ifcExtractor.extract(), ifcTilerHelper.tiling()]);
+    await Promise.all([
+      //ifcExtractor.extract(),
+      ifcTilerHelper.tiling(),
+    ]);
 
     return true;
   }
